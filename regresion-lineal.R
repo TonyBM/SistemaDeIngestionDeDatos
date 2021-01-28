496c0f60480a2c981011c161afe7313079f2dd73
tsukipedia <salma.rubiwabi@gmail.com>
2021-01-27 20:15:39 -0600

    fix a coeficientes

--- regresion-lineal.R
+++ regresion-lineal.R
@@ -2,9 +2,22 @@ regresion_lineal <- function(array_precios,array_banos,array_habitaciones,array_
   rlm<-lm(array_precios~array_banos+array_habitaciones+array_metros)
   
   intercepto<-coef(rlm)[1]
-  Q1<-coef(rlm)[2]
-  Q2<-coef(rlm)[3]
-  Q3<-coef(rlm)[4]
+  Q1<-coef(rlm, complete = FALSE)[2]
+  Q2<-coef(rlm, complete = FALSE)[3]
+  Q3<-coef(rlm, complete = FALSE)[4]
+  
+  if(is.na(intercepto)) {
+    intercepto<-0
+  }
+  if(is.na(Q1)) {
+    Q1<-0
+  }
+  if(is.na(Q2)) {
+    Q2<-0
+  }
+  if(is.na(Q3)) {
+    Q3<-0
+  }
   
   values <- c(intercepto,Q1,Q2,Q3)
   return(values)
